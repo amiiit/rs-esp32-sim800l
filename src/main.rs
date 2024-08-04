db4d5319260402364da0837482b815a3478cea43
Amit Jakubowicz <amit@Amits-MacBook-Pro.local>
2024-08-04 17:45:04 +0200

    Working state

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::thread;
+use std::io::stdin;
+use std::{io, thread};
 use std::time::Duration;
 use esp_idf_svc::hal::gpio::{AnyIOPin, Level, PinDriver};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::prelude::Hertz;
 use esp_idf_svc::hal::uart;
 use anyhow::{bail, Result};
+use esp_idf_svc::io::Write;
+use esp_idf_svc::sys::esp;
+
+use embassy_executor::Spawner;
+use embassy_time::{Timer};
+
 
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -29,7 +36,7 @@ fn main() -> Result<()> {
     thread::sleep(Duration::from_millis(1000));
     modem_powerkey.set_level(Level::High)?;
 
-    let mut uart: uart::UartDriver = uart::UartDriver::new(
+    let mut sim800l: uart::UartDriver = uart::UartDriver::new(
         peripherals.uart1,
         pins.gpio27,
         pins.gpio26,
@@ -37,7 +44,23 @@ fn main() -> Result<()> {
         Option::<AnyIOPin>::None,
         &uart::config::Config::default().baudrate(Hertz(115_200)),
     ).unwrap();
-    thread::sleep(Duration::from_millis(5000));
 
-    Ok(())
+
+    unsafe {
+        esp!(esp_idf_svc::sys::uart_driver_install(0, 512, 512, 10, std::ptr::null_mut(), 0)).unwrap();
+        esp_idf_svc::sys::esp_vfs_dev_uart_use_driver(0);
+    }
+
+    loop {
+        let mut buffer = String::new();
+        let read_result = io::stdin().read_line(&mut buffer);
+        match read_result {
+            Ok(count) => {
+                log::info!("Echo {}", buffer);
+            }
+            Err(err) => {
+                log::error!("{}", err)
+            }
+        }
+    }
 }
